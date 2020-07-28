d34616f18596bdad3c9f685c5fd267ed688be442
David Silva Jr <david_sjr@hotmail.com>
2020-07-28 13:36:18 -0300

    Concluido Capitolo 4

--- Pic/src/app/photos/photos.module.ts
+++ Pic/src/app/photos/photos.module.ts
@@ -1,12 +1,16 @@
-import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './../app-routing.module';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
 import { PhotoComponent } from './photo/photo.component';
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 
 @NgModule({
-    declarations: [ PhotoComponent],
-    exports: [ PhotoComponent ],
-    imports: [HttpClientModule]
+    declarations: [ PhotoComponent, PhotoListComponent, PhotoFormComponent],
+    imports: [HttpClientModule, CommonModule, AppRoutingModule]
 })
 
 export class PhotosModule {}
