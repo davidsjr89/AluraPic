7153bbdbaf53b08614cd9f6ae8943a54334b3128
David da S. Jr <david_sjr@hotmail.com>
2020-07-27 21:20:31 -0300

    Concluido Módulo 3

--- Pic/src/app/photos/photo/photo.component.ts
+++ Pic/src/app/photos/photo/photo.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-photo',
-  templateUrl: './photo.component.html',
-  styleUrls: ['./photo.component.css']
+  templateUrl: './photo.component.html'
 })
 export class PhotoComponent implements OnInit {
   @Input() description = '';
